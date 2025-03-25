c9155e37304b8b6a2bd93cb1e0115dfd7ceffbfb
Inayah <oneilia@vcu.edu>
2025-03-24 21:56:54 -0400

    Update Authentication.java

--- Sprint 1/Authentication.java
+++ Sprint 1/Authentication.java
@@ -1,10 +1,33 @@
+/**
+ * Medical Adherence System
+ * Sprint 1
+ * CMSC 355 - Fundamentals of Software Engineering
+ *
+ */
+
 import java.util.HashMap;
 
 public class Authentication {
     private HashMap<String, User> users;
 
+    //creates hashmap using current user
     public Authentication(){
         this.users = new HashMap<>();
+    }
+
+    /**
+     * Checks availability of username
+     * @param username
+     * @return false if username is taken, true if username is available
+     */
+    public boolean usernameAvail(String username){
+        if(users.containsKey(username)){
+            System.out.println("Username already taken.");
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
     public boolean register(String username, String password) {
@@ -17,13 +40,13 @@ public class Authentication {
         return true;
     }
 
-    public boolean login(String username, String passowrd) {
+    public boolean login(String username, String password) {
         if (!users.containsKey(username)){
             System.out.println("User not found");
             return false;
         }
         User user = users.get(username);
-        if (user.checkPassword(passowrd)) {
+        if (user.checkPassword(password)) {
             System.out.println("login successful!");
             return true;
         }
