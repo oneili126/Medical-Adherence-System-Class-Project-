4b1f5f9c4045743918000ddecc56e0d72c3ff91e
Inayah <oneilia@vcu.edu>
2025-03-26 23:29:23 -0400

    Zoya

---
+++ Sprint 1/MedicineReport.java
@@ -0,0 +1,17 @@
+public class MedicineReport {
+    public static void main(String[] args) {
+            System.out.println("Generating medicine report...");
+
+            String medication = "Aspirin";
+            String schedule = "Twice a day";
+            String adherence = "90%";
+            String sideEffect = "None";
+
+            System.out.println("Medication: " + medication);
+            System.out.println("Schedule: " + schedule);
+            System.out.println("Adherence: " + adherence);
+            System.out.println("Side Effects: " + sideEffect);
+
+            System.out.println("Report ready to view or download.");
+        }
+    }
